f0aafbe1d3744d7944db66e89b18b0803552f6cf
Birger J. Nordølum <birgerjn@gmail.com>
2018-06-30 09:01:07 +0200

    Added email

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,11 @@
 module.exports = {
   syntax: 'postcss-scss',
-  plugins: [require('tailwindcss')('./tailwind.js')]
+  plugins: [
+    require('tailwindcss')('./tailwind.js'),
+    require('postcss-preset-env'),
+    require('autoprefixer'),
+    require('cssnano')({
+      preset: 'default'
+    })
+  ]
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'assets'),
