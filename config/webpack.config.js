eab788fa385a9015f67f733f6d3d610d2f36a4f9
Birger J. Nordølum <birgerjn@gmail.com>
2018-07-03 14:55:39 +0200

    Tweaks

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -2,50 +2,59 @@ const path = require('path')
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, '../assets'),
-    filename: './js/bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(css|scss)$/,
-        use: [
-          // MiniCssExtractPlugin.loader,
-          process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1
+module.exports = (env = {}) => {
+  return {
+    mode: env.production ? 'production' : 'development',
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, '../assets'),
+      filename: './js/bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(css|scss)$/,
+          use: [
+            // MiniCssExtractPlugin.loader,
+            env.production ? MiniCssExtractPlugin.loader : 'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 1,
+                minimize: false
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                ident: 'postcss',
+                plugins: (loader) => env.production
+                  ? [
+                    require('postcss-import'),
+                    require('tailwindcss')('./config/tailwind.js'),
+                    require('postcss-preset-env'),
+                    // require('autoprefixer'),
+                    require('cssnano')
+                  ]
+                  : [
+                    require('postcss-import'),
+                    require('tailwindcss')('./config/tailwind.js'),
+                    require('postcss-preset-env')
+                  ]
+              }
             }
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              ident: 'postcss',
-              plugins: (loader) => [
-                require('postcss-import'),
-                require('tailwindcss')('./config/tailwind.js'),
-                require('postcss-preset-env'),
-                require('autoprefixer'),
-                // require('cssnano')
-              ]
-            }
-          }
-        ]
-      }
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: './css/[name].css'
+      }),
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery'
+      })
     ]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: './css/[name].css'
-    }),
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery'
-    })
-  ]
+  }
 }
