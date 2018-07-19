a39f48de247f7bf87682dd99959ac764136ed577
Birger J. Nordølum <birgerjn@gmail.com>
2018-07-19 17:51:36 +0200

    Lint and prettier

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -50,14 +50,16 @@ module.exports = () => {
         },
         {
           test: /\.(woff|woff2|eot|ttf|otf)$/,
-          use: [{
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'fonts/',
-              publicPath: '../fonts' // Take the directory into account
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]',
+                outputPath: 'fonts/',
+                publicPath: '../fonts' // Take the directory into account
+              }
             }
-          }]
+          ]
         }
       ]
     },
--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+/* global $ */
+
 // Import Fancybox
 import '@fancyapps/fancybox'
 
