62711205a0b4c1a79dab551c6e8699901ba62259
Birger J. Nordølum <contact@mindtooth.no>
2019-05-15 17:10:32 +0200

    Change the whole site to use Parcel (#154)
    I've removed Webpack for Parcel.  This reduces
    complexity a ton.
    
    Closes #135.

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+// Import Fancybox
+import '@fancyapps/fancybox'
+
+import $ from 'jquery'
+// Import jQuery Lazy
+import 'jquery-lazy'
+
+// Load jQuery Lazy
+$(function () {
+  $('.lazy').Lazy({
+    delay: 250
+  })
+})
