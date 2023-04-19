67a8bf9f0d4c3baee0df91b841d1c7e1dcc86273
Daniel Berg <mail@roosta.sh>
2023-04-19 18:47:39 +0200

    Set css vars

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import "./style.css"
+const palette = require("@srcery-colors/srcery-palette");
+
+function setVars() {
+  const vars = Object.entries(palette.primary).map(([k, v]) => {
+    return `--color-${v.termcol}: ${v.hex}`
+  })
+  document.body.setAttribute('style', vars.join(';'));
+}
+
 function main() {
-  console.log("hello world!")
+  console.log("hello world!");
+  setVars();
 }
 
 main();
