5150f8a2cc3ff4a91630634d0f5e299b04f80871
Daniel Berg <mail@roosta.sh>
2023-05-09 17:09:27 +0200

    Setup highlight js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,33 @@
-import Alpine from 'alpinejs'
+import Alpine from "alpinejs";
+
+import hljs from 'highlight.js/lib/core';
+import javascript from 'highlight.js/lib/languages/javascript';
+import c from "highlight.js/lib/languages/c"
+hljs.registerLanguage('c', c);
+hljs.registerLanguage('javascript', javascript);
+
+import "highlight.js/styles/srcery.css";
+
+import ctxt from "./examples/c.txt?raw";
+import jstxt from "./examples/js.txt?raw";
 
 function main() {
-  console.log("hello srcery!");
+
   window.Alpine = Alpine
+  Alpine.data("examples", () => ({
+    c: {
+      data: ctxt,
+      class: "language-c"
+    },
+    js: {
+      data: jstxt,
+      class: "language-js"
+    }
+  }))
+
   Alpine.start()
+
+  hljs.highlightAll();
 }
 
 main();
