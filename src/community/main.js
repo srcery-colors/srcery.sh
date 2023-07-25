95613a153cd25fb35c3a85a1e08e210f3b2f3900
Daniel Berg <mail@roosta.sh>
2023-07-25 16:45:48 +0200

    Add discord section

--- src/community/main.js
+++ src/community/main.js
@@ -1,9 +1,12 @@
 import Alpine from "alpinejs";
 import github from "../github.json";
+import discordSvg from "@fortawesome/fontawesome-free/svgs/brands/discord.svg?raw";
+
 function main() {
   window.Alpine = Alpine
   Alpine.data("state", () => ({
-    github
+    github,
+    icon: discordSvg
   }))
 
   Alpine.start()
