de1050d9be90423fffbbc98a2186ee6ac724b5a2
Daniel Berg <mail@roosta.sh>
2023-07-26 16:01:38 +0200

    Fix output dir

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { resolve } from "path"
 module.exports = defineConfig({
   root: resolve(__dirname, "src"),
   build: {
+    outDir: "../dist",
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
