2cd7b86201802774e46a4d86789c923d3568ca7a
Daniel Berg <mail@roosta.sh>
2023-08-09 18:25:06 +0200

    feat: setup handlebars & set header to partial
    So previously the header was repeated in each index, which was ok, but
    now as I plan to add more common elements templating makes more sense so
    I've setup handlebars. I've also converted the header part of each page
    to a partial that accepts arguments, that decide if the header text
    should be colored or not.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 const { defineConfig } = require("vite");
 import { resolve } from "path"
+import handlebars from 'vite-plugin-handlebars';
 
 module.exports = defineConfig({
   root: resolve(__dirname, "src"),
+  plugins: [
+    handlebars({
+      partialDirectory: resolve(__dirname, 'src/partials'),
+    }),
+  ],
   build: {
     outDir: "../dist",
     rollupOptions: {
