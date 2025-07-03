4952f40d5c10b80593caf7b3a88ae3275150c8c5
Daniel Berg <mail@roosta.sh>
2025-07-03 14:37:45 +0200

    chore: chore: enable html linting
    - Setup html-eslint, htmlhint was removed previously, this replaces it,
      without the handlebars issues (see config).
    - Fix some issues reported by linter.
    
    Additionally disable eslint rules that conflict with prettier
    formatting. Let prettier handle formatting, eslint handle linting.
    
    Refs:
    https://prettier.io/docs/comparison

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,11 @@ import { defineConfig } from "eslint/config";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 import globals from "globals";
 import js from "@eslint/js";
+import html from "@html-eslint/eslint-plugin";
+import {
+  TEMPLATE_ENGINE_SYNTAX,
+  default as htmlParser,
+} from "@html-eslint/parser";
 
 export default defineConfig([
   {
@@ -28,4 +33,24 @@ export default defineConfig([
   js.configs.recommended,
 
   eslintConfigPrettier,
+  {
+    ...html.configs["flat/recommended"],
+    files: ["**/*.html"],
+    plugins: {
+      "@html-eslint": html,
+    },
+    languageOptions: {
+      parser: htmlParser,
+      parserOptions: {
+        templateEngineSyntax: TEMPLATE_ENGINE_SYNTAX.HANDLEBAR,
+      },
+    },
+    rules: {
+      ...html.configs["flat/recommended"].rules,
+      "@html-eslint/indent": "off",
+      "@html-eslint/no-extra-spacing-attrs": "off",
+      "@html-eslint/require-closing-tags": "off",
+      "@html-eslint/attrs-newline": "off",
+    },
+  },
 ]);
