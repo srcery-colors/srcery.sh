f8b6c1ca7e8ce4677f4daf2a23f0836f2ffd1b67
Daniel Berg <mail@roosta.sh>
2026-06-26 14:32:09 +0200

    fix(astro): preserve HTML whitespace
    Latest version of astro compresses whitespace, so that our links will
    not have spaces around them

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  compressHTML: false,
   vite: {
     plugins: [tailwindcss()],
   },
